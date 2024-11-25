8156144744c629db33a395155a1e5294f08190bc
Chaitanya Tajne <164713882+chaitanyatajne@users.noreply.github.com>
2024-11-25 10:25:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,9 @@ from dotenv import load_dotenv
 
 # Load environment variables
 load_dotenv()
+LANGCHAIN_API_KEY = st.secrets['LANGCHAIN_API_KEY']
+LANGCHAIN_TRACING_V2 = "true"
+LANGCHAIN_PROJECT = "Simple Q&A Chatbot with Multiple Models"
 
 # Setting Up Environment Variables
 os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
